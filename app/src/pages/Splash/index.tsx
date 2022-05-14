2fe637cf6cc5cfbce08bec335f8163a96d7dd2d5
Sammy Au <69769431+samau3@users.noreply.github.com>
2022-05-13 22:05:57 -0700

    Deploy frontend (#6)
    * deploying on Firebase
    
    * adding automatic deployment to Firebase
    
    * Made change to splash page
    
    * updating .gitignore to include .firebase

--- app/src/pages/Splash/index.tsx
+++ app/src/pages/Splash/index.tsx
@@ -1,13 +1,8 @@
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './Splash.css'
 
 export function Splash() {
-  const [text, setText] = useState('')
-
-  useEffect(() => {
-    fetchText(setText)
-  }, [text])
 
   return (
     <div className="Splash">
@@ -16,24 +11,11 @@ export function Splash() {
         <p>
           Edit <code>src/Splash.tsx</code> and save to reload.
         </p>
-        <p>{text}</p>
-        <a
-          className="Splash-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Deployed on Firebase.
+        </p>
       </header>
     </div>
   )
 }
 
-async function fetchText(
-  setText: React.Dispatch<React.SetStateAction<string>>
-) {
-  const response = await fetch('/test')
-  const test = (await response.json())['test']
-  setText(test)
-}
