54caa2c3c96ced52bb2c72cc3f2b3c6ca22e6c3e
samau3 <samau415@gmail.com>
2022-05-16 21:51:07 -0700

    adding early design ideas for homepage

--- app/src/pages/Splash/index.tsx
+++ app/src/pages/Splash/index.tsx
@@ -1,4 +1,5 @@
 // import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import logo from './logo.svg'
 import './Splash.css'
 
@@ -7,15 +8,18 @@ export function Splash() {
   return (
     <div className="Splash">
       <header className="Splash-header">
-        <img src={logo} className="Splash-logo" alt="logo" />
+        {/* <img src={logo} className="Splash-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/Splash.tsx</code> and save to reload.
+          Some of the top performing stocks our users own:
         </p>
         <p>
-          Deployed on Firebase.
+          Insert a table
         </p>
+        <Link to="/login">Log In</Link>
       </header>
+      <p>
+        Deployed on Firebase.
+      </p>
     </div>
   )
 }
-
