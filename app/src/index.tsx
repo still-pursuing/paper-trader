73f1e7e20cf21a9e5c2d1fa09aeab21f996413de
samau3 <samau415@gmail.com>
2022-05-19 00:02:39 -0700

    trying out menu component

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Menu, Pane } from 'evergreen-ui'
+
 import './index.css'
 import { Splash } from './pages/Splash'
 import reportWebVitals from './reportWebVitals'
@@ -7,9 +9,27 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { NotFound } from './pages/NotFound'
 import Login from './pages/Login'
 
+function fakeLog() {
+  console.log("clicked logging button");
+}
 
 ReactDOM.render(
   <React.StrictMode>
+    <Pane display="flex" padding={16} background="tint2" borderRadius={3}>
+      <Pane >
+        <Menu>
+          <Menu.Group>
+            <Menu.Item onSelect={fakeLog} >Login</Menu.Item>
+            <Menu.Item>Home</Menu.Item>
+            <Menu.Item>Profile</Menu.Item>
+          </Menu.Group>
+          <Menu.Divider />
+          <Menu.Group>
+            <Menu.Item intent="danger" onSelect={fakeLog} >Logout</Menu.Item>
+          </Menu.Group>
+        </Menu>
+      </Pane>
+    </Pane>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Splash />} />
@@ -17,6 +37,7 @@ ReactDOM.render(
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
+
   </React.StrictMode>,
   document.getElementById('root')
 )
