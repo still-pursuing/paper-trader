10168a7abad38799d6ba678c171f925d16bd5363
samau3 <samau415@gmail.com>
2022-05-26 17:57:34 -0700

    Updated Navbar
    Resolved active nav selection issue by overriding default tab anchor
    Also added a brand and changed layout

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,17 +1,26 @@
 import { useState } from "react";
-import { Pane, Tab, TabNavigation } from 'evergreen-ui';
+import { Link as ReactRouterLink } from 'react-router-dom'
+import { Pane, Tab, TabNavigation, WaterfallChartIcon } from 'evergreen-ui';
+
 
 function Navbar() {
-    const [selectedIndex, setSelectedIndex] = useState(0) // currently component reloads again resetting selectedIndex
+    const [selectedIndex, setSelectedIndex] = useState(0)
     const [tabs] = useState(['Home', 'Profile', 'Login'])
     return (
-        <Pane height={40}>
-            <TabNavigation marginBottom={10} flexBasis={240} marginRight={24}>
+        <Pane flex={10} display="flex" justifyContent="space-between" marginBottom={10} >
+            <TabNavigation display="flex" alignItems="center" marginRight={10} paddingLeft={0}>
+                <Tab is={ReactRouterLink} to="/" >
+                    Paper Trader
+                    <WaterfallChartIcon size={20} marginLeft={5} />
+                </Tab>
+
+            </TabNavigation>
+            <TabNavigation alignItems="center" flexBasis={216} display="flex">
                 {tabs.map((tab, index) => (
                     <Tab
                         key={tab}
-                        is="a"
-                        href={tab}
+                        is={ReactRouterLink}
+                        to={tab}
                         id={tab}
                         onSelect={() => setSelectedIndex(index)}
                         isSelected={index === selectedIndex}>
