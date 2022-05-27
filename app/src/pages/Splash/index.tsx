3fd0da3163a0c4463ef7b37dfdbeaf5386378da7
samau3 <samau415@gmail.com>
2022-05-26 18:00:55 -0700

    Added placeholder components and text to see layout of how components will appear

--- app/src/pages/Splash/index.tsx
+++ app/src/pages/Splash/index.tsx
@@ -1,25 +1,31 @@
 // import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link as ReactRouterLink } from 'react-router-dom'
 import logo from './logo.svg'
 import './Splash.css'
+import DataTable from "../../components/DataTable"
+import { Heading, Link, Pane, Paragraph } from 'evergreen-ui'
 
 export function Splash() {
 
   return (
-    <div className="Splash">
-      <header className="Splash-header">
-        <img src={logo} className="Splash-logo" alt="logo" />
-        <p>
-          Some of the top performing stocks our users own: 📈📉
-        </p>
-        <p>
-          Insert a table
-        </p>
-        <Link to="/login">Log In</Link>
-      </header>
+    <Pane>
+      <Pane background="tint2" elevation={1} >
+        <Heading is='h1' display="flex" justifyContent="space-around" size={1}>
+          Paper Trader
+        </Heading>
+        <Paragraph display="flex" justifyContent="space-around" marginTop={10}>
+          Trading real stocks with fake money.
+        </Paragraph>
+      </Pane>
+      <Pane padding={10}>
+        <DataTable></DataTable>
+
+      </Pane>
+      <Link is={ReactRouterLink} to="/login">Discord Log In</Link>
       <p>
         Deployed on Firebase.
       </p>
-    </div>
+    </Pane >
+
   )
 }
