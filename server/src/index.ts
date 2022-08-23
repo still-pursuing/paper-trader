02666d7ec19ec35af69fa9b2cf829d4ab2fd4c81
samau3 <samau415@gmail.com>
2022-08-22 22:25:50 -0700

    Remove incorrect axios posting code

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,9 @@ import express from 'express'
 import axios from 'axios'
 
 const app = express()
-const { clientId, clientSecret, portDiscord } = require('../tsconfig.json');
+// const { clientId, clientSecret, portDiscord } = require('../tsconfig.json');
 
-const port = process.env.PORT ?? portDiscord
+const port = process.env.PORT ?? 8080
 
 async function getJSONResponse(body: any) {
   let fullBody = '';
@@ -22,34 +22,8 @@ app.get('/test', (req, res) => {
 
 app.get('/login', async (req, res) => {
   const { code } = await req.query;
-  // const query = JSON.parse(req.query);
+
   console.log(`The access code is: ${code}`);
-  // const { code } = req.query;
-
-  if (code) {
-    try {
-      const tokenResponseData = await axios({
-        method: "post",
-        url: "https://discord.com/api/oauth2/token",
-        params: {
-          client_id: clientId,
-          client_secret: clientSecret,
-          code,
-          grant_type: 'authorization_code',
-          redirect_uri: `http://localhost:${port}`,
-          scope: 'identify',
-        },
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      });
-
-      const oauthData = await getJSONResponse(tokenResponseData.data);
-      console.log(oauthData);
-    } catch (error) {
-      console.error(error);
-    }
-  }
 
   return res.json({ root: '.', req: "test" });
 })
