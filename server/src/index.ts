2cb15bfe9dcdbf5aac065de961cfb0ec8d81ee6c
samau3 <samau415@gmail.com>
2022-08-23 22:10:33 -0700

    Update /login route to make requests to Discord APIs

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,31 +1,62 @@
-import express from 'express'
-import axios from 'axios'
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
+import 'dotenv/config';
 
-const app = express()
+const app = express();
+
+// dotenv.config();
+
+app.use(cors());
 // const { clientId, clientSecret, portDiscord } = require('../tsconfig.json');
 
-const port = process.env.PORT ?? 8080
-
-async function getJSONResponse(body: any) {
-  let fullBody = '';
-
-  for await (const data of body) {
-    fullBody += data.toString();
-  }
-  return JSON.parse(fullBody);
-}
+const port = process.env.PORT ?? 8080;
 
 
 app.get('/test', (req, res) => {
+  console.log('connected to test')
   res.json({ test: 'Hello world!' })
 })
 
 app.get('/login', async (req, res) => {
   const { code } = await req.query;
-
   console.log(`The access code is: ${code}`);
 
-  return res.json({ root: '.', req: "test" });
+  if (code) {
+    let config = {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    };
+
+    try {
+      const tokenResponseData = await axios.post(
+        'https://discord.com/api/oauth2/token',
+        `client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=authorization_code&redirect_uri=http://localhost:3000/login/discord-redirect&scope=identify&code=${code}`,
+        config
+      );
+
+      const oauthData = tokenResponseData.data;
+      // console.log(oauthData);
+
+      const userResult = await axios({
+        method: 'GET',
+        url: 'https://discord.com/api/users/@me',
+        headers: {
+          authorization: `${oauthData.token_type} ${oauthData.access_token}`,
+        }
+      });
+
+      const { username, discriminator } = userResult.data;
+      console.log(`Hi ${username}${discriminator}`)
+      return res.json({ user: `${username}#${discriminator}` });
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
 })
 
 app.listen(port, () => {
