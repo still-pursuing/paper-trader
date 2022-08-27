b9b033f67eea86e1bac6d66013e9481c415a44cc
samau3 <samau415@gmail.com>
2022-08-26 21:34:05 -0700

    Moved configuration variables to separate file

---
+++ server/src/config.ts
@@ -0,0 +1,13 @@
+const port = process.env.PORT ?? 8080;
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.NODE_ENV === "production"
+    ? "https://paper-trader-182a4.web.app/login/discord-redirect"
+    : "http://localhost:3000/login/discord-redirect";
+
+export {
+    port,
+    clientId,
+    clientSecret,
+    REDIRECT_URI
+}
