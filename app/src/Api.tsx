caf3104ef6a9bd64ce5f8b685c4a48a9b0d07e0b
samau3 <samau415@gmail.com>
2022-08-27 22:26:40 -0700

    Added generateRandomString method

--- app/src/Api.tsx
+++ app/src/Api.tsx
@@ -4,6 +4,23 @@ import { BACKEND_BASE_URL } from "./config";
 
 class PaperTraderApi {
 
+    /**
+     * Generates a random string of characters
+     * 
+     * Returns:
+     *  a string of characters, e.g. e-[B~(v-l"N]Wx?9%2w(
+     */
+    static generateRandomString() {
+        let randomString = '';
+        const randomNumber = Math.floor(Math.random() * 10);
+
+        for (let i = 0; i < 20 + randomNumber; i++) {
+            randomString += String.fromCharCode(33 + Math.floor(Math.random() * 94));
+        }
+        return randomString;
+    }
+
+
     /** Makes a request to server with Discord OAuth authorization code input
      * 
      * Returns: 
