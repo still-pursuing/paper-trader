d5269711e83e0aa74f6ed9847d8c54934f3fb700
samau3 <samau415@gmail.com>
2022-09-01 19:46:53 -0700

    Start working on app wide user information

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,7 @@
-import { useEffect } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import { Pane } from 'evergreen-ui'
+import { decodeToken } from "react-jwt";
 
 import { NotFound } from './pages/NotFound'
 import Login from './pages/Login'
@@ -8,6 +9,8 @@ import Navbar from './components/Navbar';
 import { Splash } from './pages/Splash'
 import DiscordRedirect from './pages/Login/DiscordRedirect'
 import PaperTraderApi from './Api'
+import UserContext from "./UserContext";
+
 
 /**
  * Props:
@@ -23,32 +26,67 @@ import PaperTraderApi from './Api'
  */
 
 function App() {
+    const [token, setToken] = useState(localStorage.getItem('userToken'));
+    const [needsRedirect, setNeedsRedirect] = useState(false);
+    const [currentUser, setCurrentUser] = useState(null);
 
     /** 
-    * Stores string generated from PaperTraderApi in localStorage to
-    * use for Discord OAUth CSRF prevention when component mounts if there
-    * no token
-    */
+      * Stores string generated from PaperTraderApi in localStorage to
+      * use for Discord OAUth CSRF prevention when component mounts if there
+      * no token
+      */
     useEffect(function storeCsrfTokenOnMount() {
         if (localStorage['stateString'] === undefined) {
             const randomString = PaperTraderApi.generateRandomString();
             localStorage.setItem('stateString', randomString);
-            console.log('inside effect:', localStorage['stateString'])
         }
-    }, []);
+
+        async function getCurrentUser() {
+            if (token !== null) {
+                try {
+                    let decodedToken = decodeToken(token);
+                    console.log('decoded', decodedToken);
+                    let jsonToken = JSON.stringify(decodedToken);
+                    let parsed = JSON.parse(jsonToken);
+                    if (parsed !== null) {
+                        const username = parsed.username
+                        PaperTraderApi.token = token;
+                        let resultUser = await PaperTraderApi.getCurrentUser(username)
+                        console.log({ resultUser })
+                        setCurrentUser(resultUser);
+                        setNeedsRedirect(false);
+                    } else {
+                        throw new Error('Invalid username');
+                    }
+                } catch (err) {
+                    console.error("Can't load user", err);
+                }
+            }
+        }
+        getCurrentUser();
+        console.log('In App effect')
+    }, [token]);
+
+    console.log({ currentUser });
+
+    if (needsRedirect) {
+        return <Navigate replace to="/" />
+    }
 
     return (
-        <Pane padding={16}>
-            <Navbar></Navbar>
-            <Routes>
-                <Route path="/" element={<Splash />} />
-                <Route path="home" element={<Splash />} />
-                <Route path="login" element={<Login />}>
-                    <Route path="discord-redirect" element={<DiscordRedirect />} />
-                </Route>
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </Pane>
+        <UserContext.Provider value={currentUser}>
+            <Pane padding={16}>
+                <Navbar></Navbar>
+                <Routes>
+                    <Route path="/" element={<Splash />} />
+                    <Route path="home" element={<Splash />} />
+                    <Route path="login" element={<Login />}>
+                        <Route path="discord-redirect" element={<DiscordRedirect />} />
+                    </Route>
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </Pane>
+        </UserContext.Provider>
     )
 }
 
