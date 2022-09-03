bfab1b77033fb31d725c779bf4ed1c4e53555e7f
samau3 <samau415@gmail.com>
2022-09-02 17:12:29 -0700

    Move function inside useEffect to be passed down from App

--- app/src/pages/Login/DiscordRedirect.tsx
+++ app/src/pages/Login/DiscordRedirect.tsx
@@ -24,47 +24,17 @@ interface token {
  * Login --> DiscordRedirect
  */
 
-function DiscordRedirect() {
-    const [searchParams] = useSearchParams();
-    const [user, setUser] = useState('');
+function DiscordRedirect({ handleLogin }: any) {
+    // const [searchParams] = useSearchParams();
+    // const [user, setUser] = useState('');
 
     /**
      * Validates if there is no CSRF attack and authenticates Discord user with
      * a valid OAuth code by communicating with PaperTraderApi
      */
     useEffect(function validateUserOnSearchParams() {
-        async function validateUser() {
-            try {
-                if (localStorage['stateString'] !== searchParams.get('state')) {
-                    throw new Error("Clickjacked!!");
-                }
-
-                const discordOAuthCode = searchParams.get('code');
-
-                // todo: move this into a handleLogin function that gets passed down
-                if (discordOAuthCode) {
-                    const token = await PaperTraderApi.getDiscordUser(discordOAuthCode);
-                    localStorage.setItem('userToken', token);
-
-                    const decodedToken = decodeToken<token>(token);
-                    if (decodedToken !== null) {
-                        const username = decodedToken.username;
-                        setUser(username);
-                    }
-                    // const username = decodedToken !== null ? decodedToken.username : "";
-                    // const username = decodedToken?.username; //optional chaining
-                    // console.log(decodedToken);
-                    // setUser(username);
-
-                } else {
-                    throw new Error('Missing Discord OAuth code');
-                }
-            } catch (err) {
-                console.error(err);
-            }
-        }
-        validateUser();
-    }, [searchParams]);
+        handleLogin();
+    }, []);
 
     return (
         <Pane display="flex" flexDirection="column" alignItems="center">
