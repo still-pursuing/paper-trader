365c8a6c7b3dfb881c1f9f75af6192984cee6aac
samau3 <samau415@gmail.com>
2022-09-05 23:21:53 -0700

    Update redirect URIs

--- app/src/config.ts
+++ app/src/config.ts
@@ -3,7 +3,7 @@ const BACKEND_BASE_URL = process.env.NODE_ENV === "production"
   : "http://localhost:8080";
 
 const DISCORD_REDIRECT_URI = process.env.NODE_ENV === "production"
-  ? "https://paper-trader-182a4.web.app/login/discord-redirect"
-  : "http://localhost:3000/login/discord-redirect";
+  ? "https://paper-trader-182a4.web.app/login"
+  : "http://localhost:3000/login";
 
 export {BACKEND_BASE_URL, DISCORD_REDIRECT_URI};
--- server/src/config.ts
+++ server/src/config.ts
@@ -8,8 +8,8 @@ const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 
 const REDIRECT_URI = process.env.NODE_ENV === "production"
-    ? "https://paper-trader-182a4.web.app/login/discord-redirect"
-    : "http://localhost:3000/login/discord-redirect";
+    ? "https://paper-trader-182a4.web.app/login"
+    : "http://localhost:3000/login";
 
 export {
     SECRET_KEY,
