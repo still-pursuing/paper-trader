ee4a099eb740694a608a2c761a7221e8aa6b64a7
samau3 <samau415@gmail.com>
2022-09-07 22:17:57 -0700

    Rename SECRET_KEY and export the variables individually

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,20 +1,12 @@
 import 'dotenv/config';
 
-const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+export const JWT_SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
 
-const port = process.env.PORT ?? 8080;
+export const port = process.env.PORT ?? 8080;
 
-const clientId = process.env.CLIENT_ID;
-const clientSecret = process.env.CLIENT_SECRET;
+export const clientId = process.env.CLIENT_ID;
+export const clientSecret = process.env.CLIENT_SECRET;
 
-const REDIRECT_URI = process.env.NODE_ENV === "production"
+export const REDIRECT_URI = process.env.NODE_ENV === "production"
     ? "https://paper-trader-182a4.web.app/login"
     : "http://localhost:3000/login";
-
-export {
-    SECRET_KEY,
-    port,
-    clientId,
-    clientSecret,
-    REDIRECT_URI
-};
