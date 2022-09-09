dd88d17478a2ba4b57353eb5a2034bd8426875ce
samau3 <samau415@gmail.com>
2022-09-08 20:52:52 -0700

    Change process of extracting JWT from auth header to not use regex

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -14,7 +14,7 @@ function authenticateJWT(req: express.Request, res: express.Response, next: expr
     try {
         const authHeader = req.headers && req.headers.authorization;
         if (authHeader) {
-            const token = authHeader.replace(/^[Bb]earer /, "").trim();
+            const token = authHeader.split(" ")[1];
             const verifiedUser = jwt.verify(token, JWT_SECRET_KEY)
             if (typeof verifiedUser !== 'string') {
                 res.locals.user = verifiedUser.username;
