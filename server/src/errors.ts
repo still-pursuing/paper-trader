606fb5764263c6eeeca06d10383ce71aa049206e
Sammy Au <69769431+samau3@users.noreply.github.com>
2022-09-29 17:05:30 -0700

    Add express errors (#23)
    * Create custom error classes
    
    * Add Error routes and change login route to use generic axios requests
    
    * Use generic messsage if there's a 500 error
    
    * Redact error properties before being passed to error handler
    
    * Add error if code parameter not provided
    
    * Remove test route and add typing to vars that use axios responses
    
    * Add missed typing for Discord Users route
    
    * Simplify URLSearchParams creation and make check for code query sooner
    
    * Separate nested try catch block

---
+++ server/src/errors.ts
@@ -0,0 +1,65 @@
+interface configProperties {
+    headers: headers,
+    method: string,
+    url: string,
+    data: string
+}
+
+interface headers {
+    Accept: string,
+    authorization: string
+}
+
+interface request {
+    _header: string
+}
+
+/** ExpressError extends normal JS error so we can
+ *  add a status when we make an instance of it.
+ *
+ *  The error-handling middleware will return this.
+ */
+
+export class ExpressError extends Error {
+    status: number;
+    headers: headers;
+    config: configProperties;
+    request: request;
+
+    constructor(message: string, status: number) {
+        super(message);
+        this.status = status;
+    }
+}
+
+/** 404 NOT FOUND error. */
+
+export class NotFoundError extends ExpressError {
+    constructor(message = "Not Found") {
+        super(message, 404);
+    }
+}
+
+/** 401 UNAUTHORIZED error. */
+
+export class UnauthorizedError extends ExpressError {
+    constructor(message = "Unauthorized") {
+        super(message, 401);
+    }
+}
+
+/** 400 BAD REQUEST error. */
+
+export class BadRequestError extends ExpressError {
+    constructor(message = "Bad Request") {
+        super(message, 400);
+    }
+}
+
+/** 403 BAD REQUEST error. */
+
+export class ForbiddenError extends ExpressError {
+    constructor(message = "Forbidden") {
+        super(message, 403);
+    }
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import axios from 'axios';
 import cors from 'cors';
+import morgan from 'morgan';
 
 import {
   port,
@@ -11,65 +12,109 @@ import {
 
 import { createToken } from '../helpers/token';
 import { authenticateJWT, ensureCorrectUser } from '../middleware/auth';
+import { BadRequestError, ExpressError, NotFoundError } from './errors';
+
+interface DiscordOAuthTokenResponseData {
+  token_type: string
+  access_token: string
+}
+
+interface DiscordUserData {
+  username: string
+  discriminator: string
+}
 
 const app = express();
 
 app.use(cors());
+app.use(morgan('tiny'));
 
-app.get('/test', (req, res) => {
-  console.log('connected to test')
-  res.json({ test: 'Hello world!' })
-})
+app.get('/login', async (req, res, next) => {
+  const { code } = req.query;
 
-app.get('/login', async (req, res) => {
-  const { code } = await req.query;
-  console.log(`The access code is: ${code}`);
+  if (!code) {
+    return next(new BadRequestError());
+  }
 
-  if (code) {
-    const config = {
+  let oauthTokenData: DiscordOAuthTokenResponseData;
+
+  try {
+    const params = new URLSearchParams({
+      client_id: clientId,
+      client_secret: clientSecret,
+      code: code.toString(),
+      grant_type: 'authorization_code',
+      redirect_uri: REDIRECT_URI,
+      scope: 'identify'
+    });
+
+    oauthTokenData = (await axios({
+      method: 'POST',
+      url: 'https://discord.com/api/oauth2/token',
+      data: params,
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    };
+      },
+    })).data;
+  } catch (error) {
+    const { data } = error.response.config;
+    error.response.config.data =
+      `client_id=REDACTED&client_secret=REDACTED&${data.substring(data.indexOf("grant_type"))}`;
 
-    try {
-      const tokenResponseData = await axios.post(
-        'https://discord.com/api/oauth2/token',
-        `client_id=${clientId}&client_secret=${clientSecret}&grant_type=authorization_code&redirect_uri=${REDIRECT_URI}&scope=identify&code=${code}`,
-        config
-      );
-
-      const oauthData = tokenResponseData.data;
-
-      const userResult = await axios({
-        method: 'GET',
-        url: 'https://discord.com/api/users/@me',
-        headers: {
-          authorization: `${oauthData.token_type} ${oauthData.access_token}`,
-        }
-      });
-
-      const { username, discriminator } = userResult.data;
-      console.log(`Hi ${username}${discriminator}`)
-
-      const token = createToken(`${username}${discriminator}`);
-      return res.json({ token })
-    } catch (error) {
-      console.error(error);
-    }
+    error.response.message = error.response.data.error_description;
+    return next(error.response);
   }
+
+  try {
+    const userResult: DiscordUserData = (await axios({
+      method: 'GET',
+      url: 'https://discord.com/api/users/@me',
+      headers: {
+        authorization: `${oauthTokenData.token_type} ${oauthTokenData.access_token}`,
+      }
+    })).data;
+
+    const { username, discriminator } = userResult;
+
+    const token = createToken(`${username}${discriminator}`);
+    return res.json({ token });
+  } catch (error) {
+
+    error.response.config.headers.authorization = "Bearer REDACTED";
+    error.response.request._header = "REDACTED";
+
+    error.response.message = error.response.data.message;
+    return next(error.response);
+  }
+
 })
 
-app.get('/profile', authenticateJWT, ensureCorrectUser, async (req, res) => {
+app.get('/profile', authenticateJWT, ensureCorrectUser, async (req, res, next) => {
   try {
     // query database for user's data in the future?
-
     const user = res.locals.user;
     return res.json({ user });
   } catch (err) {
-    return res.json({ user: 'Invalid username' });
+    return next(err);
   }
 })
+
+/** Handle 404 errors -- this matches everything */
+app.use((req, res, next) => {
+  return next(new NotFoundError());
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use((err: ExpressError, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  const status = err.status || 500;
+  const message = err.status < 500 ? err.message : "Something went wrong";
+
+  console.error(err);
+
+  return res.status(status).json({
+    error: { message, status },
+  });
+});
 
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`)
