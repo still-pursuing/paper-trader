766d8fb77333d46226e45b3a1887cde7e986f193
Sammy Au <69769431+samau3@users.noreply.github.com>
2022-10-04 19:42:33 -0700

    Update React client to handle errors (#24)
    * Add error alerts when login process fails
    
    * Add catch and error alert if restoring user session fails
    
    * Add docstrings to UserSession methods

--- app/src/helpers/UserSession.ts
+++ app/src/helpers/UserSession.ts
@@ -1,32 +1,30 @@
 import PaperTraderApi from "./PaperTraderApi";
 import {v4 as uuidv4} from "uuid";
+import { CsrfStateError } from "../errors/errors";
 
 class UserSession {
 	static storedCsrfStateString = localStorage.getItem('csrfStateString') ?? undefined;
 
+	/** Generate a random string using UUID and store in localStorage if needed */
 	static storeCsrfStateString() {
 		if (!this.storedCsrfStateString) {
 			localStorage.setItem('csrfStateString', uuidv4());
 		}
 	};
 
+	/** Check if there's a user session to restore or start */
 	static async getCurrentUser(token: string | undefined) {
 		if (token !== undefined) {
-			try {
 				PaperTraderApi.token = token;
-				let resultUser = await PaperTraderApi.getCurrentUser()
+				let resultUser = await PaperTraderApi.getCurrentUser();
 				return resultUser;
-			} catch (err) {
-				// todo: Change to properly throw an Error?
-				console.error("Can't load user", err);
-			}
 		}
 	};
 	
+	/** Make a request to Discord to get user data */
 	static async login(searchParams: URLSearchParams) {
-		try {
 			if (this.storedCsrfStateString !== searchParams.get('state')) {
-				throw new Error("Clickjacked!!");
+				throw new CsrfStateError();
 			}
 
 			const discordOAuthCode = searchParams.get('code');
@@ -35,13 +33,7 @@ class UserSession {
 				const token = await PaperTraderApi.getDiscordUser(discordOAuthCode);
 				localStorage.setItem('userToken', token);
 				return token;
-			} else {
-				throw new Error('Missing Discord OAuth code');
-			}
-		} catch (err) {
-			// todo: Change to properly throw an Error
-			console.error(err);
-		}
+			} 
 	};
 }
 
