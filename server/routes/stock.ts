37faa340f21d8dc6dae69a2c494e347ae353c31f
samau3 <samau415@gmail.com>
2022-10-06 18:00:53 -0700

    Add starting sell route

--- server/routes/stock.ts
+++ server/routes/stock.ts
@@ -38,10 +38,31 @@ router.post('/buy', async (req, res, next) => {
       throw new BadRequestError("Invalid Stock Ticker");
     }
 
-    const cost: number = stock.c;
-    const total = Number((cost * qty).toFixed(2));
+    const price: number = stock.c;
+    const total = Number((price * qty).toFixed(2));
 
-    return res.json({ cost, qty, total });
+    return res.json({ price, qty, total });
+  } catch (err) {
+    return next(err);
+  }
+})
+
+router.post('/sell', async (req, res, next) => {
+  const { ticker, quantity } = req.body;
+  const qty = Number(quantity);
+
+  if (!ticker) return next(new BadRequestError());
+
+  try {
+    const stock = await Finnhub.getStockQuote(ticker.toString().toUpperCase());
+
+    if (stock.c === 0) {
+      throw new BadRequestError("Invalid Stock Ticker");
+    }
+
+    const price: number = stock.c;
+
+    return res.json({ price, qty });
   } catch (err) {
     return next(err);
   }
