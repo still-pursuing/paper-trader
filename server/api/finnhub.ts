483efded132a85bd200673098f1e8c1f624e6d93
samau3 <samau415@gmail.com>
2022-10-06 22:45:13 -0700

    Add docstrings and typing to getStockQuote return

--- server/api/finnhub.ts
+++ server/api/finnhub.ts
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
-import { finnHubToken } from '../src/config';
+import { finnHubToken } from '../config';
 
 const BASE_FINNHUB_API_URL = 'https://finnhub.io/api/v1/quote';
 
+interface FinnhubQuote {
+  c: number
+}
+
 export class Finnhub {
 
+  /** Axios request builder */
   static async request(ticker: string, method = "GET") {
     const url = `${BASE_FINNHUB_API_URL}?symbol=${ticker}&token=${finnHubToken}`;
 
@@ -13,7 +18,10 @@ export class Finnhub {
     return response;
   }
 
-  static async getStockQuote(ticker: string) {
+  // Individual Finnhub API routes
+
+  /** Get a stock quote based on ticker input */
+  static async getStockQuote(ticker: string): Promise<FinnhubQuote> {
     const res = await this.request(ticker);
     return res;
   }
