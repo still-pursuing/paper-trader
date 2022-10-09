0e6730a6068c1b8f03d5c23921abee43f188d047
samau3 <samau415@gmail.com>
2022-10-09 15:15:22 -0700

    Start postgreSQL db schema

---
+++ server/paper-trader-schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE users (
+  username VARCHAR(36) PRIMARY KEY,
+  balance NUMERIC
+  is_admin BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE transactions (
+  id SERIAL PRIMARY KEY,
+  ticker TEXT NOT NULL,
+  quantity NUMERIC CHECK (quantity >= 0),
+  price NUMERIC CHECK (salary >= 0),
+  DATETIME,
+  username VARCHAR(25) NOT NULL
+    REFERENCES users ON DELETE CASCADE
+);
+
+CREATE TABLE stocks_owned (
+  PRIMARY KEY (username, ticker)
+  username VARCHAR(25)
+    REFERENCES users ON DELETE CASCADE,
+  ticker TEXT NOT NULL,
+  quantity NUMERIC,
+  avg_cost NUMERIC,
+  total_cost NUMERIC,
+  total_value NUMERIC
+
+);
+
+CREATE VIEW stocks_owned AS
+  SELECT ticker, SUM(quantity) AS total_shares, AVG(price) AS avg_cost, (total_shares * avg_cost) AS total_cost
+  FROM transactions
+  GROUP BY ticker
+);
