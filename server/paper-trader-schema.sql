b9acbd33e38c9e64fc3aa42a19205c00311a7b66
samau3 <samau415@gmail.com>
2022-10-09 16:56:07 -0700

    Create starter queries for forming table schemas

--- server/paper-trader-schema.sql
+++ server/paper-trader-schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users (
   username VARCHAR(36) PRIMARY KEY,
-  balance NUMERIC
+  balance NUMERIC,
   is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
 
@@ -8,26 +8,25 @@ CREATE TABLE transactions (
   id SERIAL PRIMARY KEY,
   ticker TEXT NOT NULL,
   quantity NUMERIC CHECK (quantity >= 0),
-  price NUMERIC CHECK (salary >= 0),
-  DATETIME,
+  price NUMERIC CHECK (price >= 0),
+  transction_time TIMESTAMP,
   username VARCHAR(25) NOT NULL
     REFERENCES users ON DELETE CASCADE
 );
 
-CREATE TABLE stocks_owned (
-  PRIMARY KEY (username, ticker)
-  username VARCHAR(25)
-    REFERENCES users ON DELETE CASCADE,
-  ticker TEXT NOT NULL,
-  quantity NUMERIC,
-  avg_cost NUMERIC,
-  total_cost NUMERIC,
-  total_value NUMERIC
+-- CREATE TABLE stocks_owned (
+--   PRIMARY KEY (username, ticker)
+--   username VARCHAR(25)
+--     REFERENCES users ON DELETE CASCADE,
+--   ticker TEXT NOT NULL,
+--   quantity NUMERIC,
+--   avg_cost NUMERIC,
+--   total_cost NUMERIC,
+--   total_value NUMERIC
+-- );
 
-);
-
-CREATE VIEW stocks_owned AS
-  SELECT ticker, SUM(quantity) AS total_shares, AVG(price) AS avg_cost, (total_shares * avg_cost) AS total_cost
-  FROM transactions
-  GROUP BY ticker
-);
+-- CREATE VIEW stocks_owned AS
+--   SELECT ticker, SUM(quantity) AS total_shares, AVG(price) AS avg_cost, (total_shares * avg_cost) AS total_cost
+--   FROM transactions
+--   GROUP BY ticker
+-- );
