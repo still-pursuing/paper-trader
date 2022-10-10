53bc54853b0faebee610de95b9045e1f609ea383
samau3 <samau415@gmail.com>
2022-10-09 17:01:07 -0700

    Update profile route to get user information from database

--- server/src/routes/profile.ts
+++ server/src/routes/profile.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { User } from "../models/user";
 
 import { ensureCorrectUser } from '../middleware/auth';
 
@@ -14,7 +15,8 @@ export const router = Router();
 router.get('/', ensureCorrectUser, async (req, res, next) => {
 	try {
 		// query database for user's data in the future?
-		const user = res.locals.user;
+		const user = await User.getUser(res.locals.user);
+		console.log('Profile', user);
 		return res.json({ user });
 	} catch (err) {
 		return next(err);
