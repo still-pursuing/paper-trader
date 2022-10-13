62d0f63a81094b1648a14399270b00ac3d9f5fef
samau3 <samau415@gmail.com>
2022-10-12 20:50:29 -0700

    Remove unnecessary route

--- server/src/routes/profile.ts
+++ server/src/routes/profile.ts
@@ -1,31 +1,18 @@
 import { Router } from "express";
 import { User } from "../models/user";
 
-import { ensureCorrectUser } from '../middleware/auth';
-
 export const router = Router();
 
 /** GET / => { user }
  *
  * Returns user's username
  *
- * Authorization required: ensureCorrectUser
  */
 
-router.get('/username', ensureCorrectUser, async (req, res, next) => {
+router.get('/', async (req, res, next) => {
 	try {
-		const user = await User.login(res.locals.user);
-		return res.json({ user });
-	} catch (err) {
-		return next(err);
-	}
-})
-
-router.get('/summary', ensureCorrectUser, async (req, res, next) => {
-	try {
-		// query database for user's data in the future?
-		const balance = await User.getBalance(res.locals.user);
-		return res.json({ balance });
+		const userPortfilio = await User.getProfile(res.locals.user);
+		return res.json({ userPortfilio });
 	} catch (err) {
 		return next(err);
 	}
