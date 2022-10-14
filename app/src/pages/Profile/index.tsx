ea3f1acdd9328303c4f1ecb08a0210c5b6351429
samau3 <samau415@gmail.com>
2022-10-13 20:21:31 -0700

    Update profile page to display user's info from database

--- app/src/pages/Profile/index.tsx
+++ app/src/pages/Profile/index.tsx
@@ -1,7 +1,7 @@
-
 import { Pane, Heading, Paragraph } from "evergreen-ui";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+import PaperTraderApi from "../../helpers/PaperTraderApi";
 
 import UserContext from "../../UserContext";
 
@@ -18,8 +18,24 @@ import UserContext from "../../UserContext";
  * Routes --> Profile
  */
 
+interface Portfolio {
+    username: string,
+    balance: string
+}
+
 function Profile() {
     const user = useContext(UserContext);
+    const [portfolio, setPortfolio] = useState<Portfolio | undefined>(undefined);
+
+    // make requests to get username and balance, transactions
+    // add a try catch in case accessing db has issues
+    useEffect(() => {
+        async function loadPortfolio() {
+            const userProfile = await PaperTraderApi.getUserProfile();
+            setPortfolio(userProfile);
+        }
+        if (user) loadPortfolio();
+    }, [user])
 
     if (!user) return <Navigate to="/login" replace />
 
@@ -28,7 +44,9 @@ function Profile() {
             <Heading is="h1" size={900}>
                 Profile Page
             </Heading>
-            <Paragraph> Hi <>{user}!</> </Paragraph>
+            {portfolio && <Paragraph> Hi <>{portfolio.username}!</> </Paragraph>}
+            {portfolio && <Paragraph> You have <>${portfolio.balance} available funds to trade with!</> </Paragraph>}
+
         </Pane >
     );
 }
