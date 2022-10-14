3fd9db7ffa38c8b3f3b6bd333b8eb0de2a5f9b17
samau3 <samau415@gmail.com>
2022-10-14 14:18:51 -0700

    Add error to be thrown if user not in db between user sessions

--- server/src/routes/profile.ts
+++ server/src/routes/profile.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { BadRequestError } from "../errors";
 import { User } from "../models/user";
 
 export const router = Router();
@@ -12,6 +13,9 @@ export const router = Router();
 router.get('/', async (req, res, next) => {
   try {
     const userPortfolio = await User.getProfile(res.locals.user);
+    if (userPortfolio === undefined) {
+      throw new BadRequestError("No user found")
+    }
     return res.json({ userPortfolio });
   } catch (err) {
     return next(err);
