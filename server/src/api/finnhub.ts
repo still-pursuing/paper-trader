1b3f8549bede540282792d241ab487e0f302e7eb
samau3 <samau415@gmail.com>
2023-01-10 22:51:59 -0800

    Refactor stock api so it's not as closely tied to Finnhub

--- server/src/api/finnhub.ts
+++ server/src/api/finnhub.ts
@@ -1,14 +1,9 @@
 import axios from 'axios';
 
 import { finnHubToken } from '../config';
+import { BASE_FINNHUB_API_URL } from '../config';
 
-const BASE_FINNHUB_API_URL = 'https://finnhub.io/api/v1/quote';
-
-interface FinnhubQuote {
-  c: number; // current price of the stock symbol provided
-}
-
-export class Finnhub {
+export class StockTransactionAPI {
   /** Axios request builder */
   static async request(ticker: string, method = 'GET') {
     const url = `${BASE_FINNHUB_API_URL}?symbol=${ticker}&token=${finnHubToken}`;
@@ -20,7 +15,8 @@ export class Finnhub {
   // Individual Finnhub API routes
 
   /** Get a stock quote based on ticker input */
-  static async getStockQuote(ticker: string): Promise<FinnhubQuote> {
-    return this.request(ticker);
+  static async getStockQuote(ticker: string): Promise<number> {
+    const { c: sharePrice } = await this.request(ticker);
+    return sharePrice;
   }
 }
