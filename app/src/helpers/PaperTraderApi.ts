aacad5b01b314c159f2df864d5c49f89c37edb15
samau3 <samau415@gmail.com>
2023-01-19 11:14:14 -0800

    Change fn name to be more clear about stock prices

--- app/src/helpers/PaperTraderApi.ts
+++ app/src/helpers/PaperTraderApi.ts
@@ -75,7 +75,7 @@ export default class PaperTraderApi {
   /**
    * Makes a request to server with stock ticker to get a quote
    */
-  static async getStock(ticker: string): Promise<number> {
+  static async getStockPrice(ticker: string): Promise<number> {
     const res = (await this.request(`stock/search?ticker=${ticker}`)).price;
 
     return res;
